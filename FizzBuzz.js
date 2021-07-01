146116636c6a95e887df26d9ad342e60057d3b73
Jordan <64916602+JordanInsights@users.noreply.github.com>
2021-07-01 12:07:22 +0100

    Add FizzBuzz.js file to the repo

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 100; i++) {
+  let divisibleByThree = i % 3 == 0;
+  let divisibleByFive = i % 5 == 0;
+  
+  if (!divisibleByFive && !divisibleByThree) {
+    console.log(i);
+  } else if (divisibleByFive && divisibleByThree) {
+    console.log('FizzBuzz');
+  } else if (divisibleByThree) {
+    console.log('Fizz');
+  } else {
+    console.log('Buzz');
+  }
+}
+
+return;
