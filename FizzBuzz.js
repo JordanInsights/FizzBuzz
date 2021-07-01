e83dc763c7f45ed2851a889dc24a7aa2f178b824
Jordan <64916602+JordanInsights@users.noreply.github.com>
2021-07-01 12:32:21 +0100

    Change the syntax of the code slightly.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,16 +1,18 @@
-for (let i = 1; i <= 100; i++) {
-  let divisibleByThree = i % 3 == 0;
-  let divisibleByFive = i % 5 == 0;
-  
-  if (!divisibleByFive && !divisibleByThree) {
-    console.log(i);
-  } else if (divisibleByFive && divisibleByThree) {
-    console.log('FizzBuzz');
-  } else if (divisibleByThree) {
-    console.log('Fizz');
-  } else {
-    console.log('Buzz');
-  }
-}
+const fizzBuzzFunc = () => {
+  for (let i = 1; i <= 100; i++) {
+    let divisibleByThree = i % 3 == 0;
+    let divisibleByFive = i % 5 == 0;
 
-return;
+    if (!divisibleByFive && !divisibleByThree) {
+      console.log(i);
+    } else if (divisibleByFive && divisibleByThree) {
+      console.log('FizzBuzz');
+    } else if (divisibleByThree) {
+      console.log('Fizz');
+    } else {
+      console.log('Buzz');
+    }
+  };
+};
+
+fizzBuzzFunc();
